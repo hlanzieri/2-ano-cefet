779b943371d395d0b4825256119e1d8aacdeadf2
Humberto de Moraes Lanzieri Filho <134455113+hlanzieri@users.noreply.github.com>
2024-03-01 08:35:55 -0300

    Update calc.java

--- calc.java
+++ calc.java
@@ -1,25 +1,40 @@
 import java.util.Scanner;
 
-public class bask{
+public class calc{
     public static void main(String args[]){
         Scanner entrada = new Scanner(System.in);
 
-        System.out.println("A seguir insira os coeficientes da sua equação:");
-        System.out.println("Insira 'a':");
-        double a = entrada.nextDouble();
-        System.out.println("Insira 'b':");
-        double b = entrada.nextDouble();
-        System.out.println("Insira 'c':");
-        double c = entrada.nextDouble();
-        double delta = b * b - 4 * a * c;
-        if (delta < 0) {
-            System.out.println("Não possui raízes reais.");
-        } else if (delta == 0) {
-            System.out.println("Possui duas raízes reais e iguais.");
-            System.out.println("O valor de X é " + (-b + Math.sqrt(delta)) / (2 * a));
-        } else {
-            System.out.println("Possui duas raízes reais diferentes.");
-            System.out.println("X pode assumir " + ((-b + Math.sqrt(delta)) / (2 * a)) + " ou " + ((-b - Math.sqrt(delta)) / (2 * a)));
+        while(true){
+        System.out.println("Insira o primeiro número:");
+        double n1 = entrada.nextDouble();
+        System.out.println("Insira o segundo número:");
+        double n2 = entrada.nextDouble();
+
+		System.out.println("Insira qual tipo de operação iremos realizar:");
+        System.out.println("1. Soma;");
+        System.out.println("2. Subtração;");
+        System.out.println("3. Multiplicação;");
+        System.out.println("4. Divisão;");
+        System.out.println("5. Sair.");
+        int resp = entrada.nextInt();
+        if(resp==5){
+            break;
         }
+
+        switch(resp){
+            case 1: 
+            System.out.println(n1 + n2);
+            break;
+            case 2:
+            System.out.println(n1-n2);
+            break;
+            case 3:
+            System.out.println(n1*n2);
+            break;
+            case 4:
+            System.out.println(n1/n2);
+        }
+    }
+    entrada.close();
 	}
 }
