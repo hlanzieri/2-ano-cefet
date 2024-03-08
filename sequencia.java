a01b5a58da75b4fc7a2d048d373f2b6be0a54977
Humberto de Moraes Lanzieri Filho <134455113+hlanzieri@users.noreply.github.com>
2024-03-08 08:33:30 -0300

    Update sequencia.java

--- sequencia.java
+++ sequencia.java
@@ -4,14 +4,16 @@ import java.util.Scanner;
 public class sequencia {
     public static void main(String args[]){
         Scanner entrada = new Scanner(System.in);
-        double n[] = new double[10];
-        Arrays.fill(n, 0);
-        for(int i=0; i<10; i++){
+        System.err.println("Insira a quantidade de vetores que o usuario deseja:");
+        int x = entrada.nextInt();
+        double n[] = new double[x];
+        Arrays.fill(n, x);
+        for(int i=0; i<x; i++){
         System.out.println("Insira o "+(i+1)+ "º número:");
         n[i] = entrada.nextDouble();
         }
         System.out.println("\nSequência inversa:\n");
-        for(int i=9; i>=0; i--){
+        for(int i=x-1; i>=0; i--){
         System.out.println(+n[i]);
         }
         entrada.close();
