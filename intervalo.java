2a66e633315872de40d71a92701babe76a8c9b26
Humberto de Moraes Lanzieri Filho <134455113+hlanzieri@users.noreply.github.com>
2024-03-08 08:11:23 -0300

    Update intervalo.java

--- intervalo.java
+++ intervalo.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ex3 {
+public class intervalo {
     public static void main(String args[]){
         Scanner entrada = new Scanner(System.in);
         System.out.println("Insira o primeiro número:");
