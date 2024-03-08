afdcda0b711476e0db073bcbe898227660d75ef3
Humberto de Moraes Lanzieri Filho <134455113+hlanzieri@users.noreply.github.com>
2024-03-08 08:41:22 -0300

    Create ex4.java
    terminar.

---
+++ ex4.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ex4 {
+    public static void main(String args[]){
+        Scanner entrada = new Scanner(System.in);
+        char m[][]={{*, *, *, *, *}, {*, *, *, *, *}, {*, *, *, *, *}, {*, *, *, *, *}};
+        for(int vet: m){
+            for(int element:vet){
+                System.out.println(element+ " ");
+                System.out.println();
+            }
+        }
+        entrada.close();
+    }
+}
