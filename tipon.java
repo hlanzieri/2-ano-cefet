9aa4d0fd949f2f23aa1e76112f6b93628f9400b7
Humberto de Moraes Lanzieri Filho <134455113+hlanzieri@users.noreply.github.com>
2024-03-08 08:10:19 -0300

    Update and rename ex2.java to tipon.java

--- tipon.java
+++ tipon.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ex2{
+public class tipon{
     public static void main(String args[]){
         Scanner entrada = new Scanner(System.in);
         while(true){
