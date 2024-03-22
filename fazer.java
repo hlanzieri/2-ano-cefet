e3802a524623a8cb37779bd6dad4828fce5fe290
Humberto de Moraes Lanzieri Filho <134455113+hlanzieri@users.noreply.github.com>
2024-03-22 09:24:37 -0300

    Delete fazer.java

--- fazer.java
+++
@@ -1,3 +0,0 @@
-/*Faça um programa que preencha (via estrutura de repetição) um vetor numérico de tamanho N em ordem crescente. Em seguida receba um valor do teclado (número inteiro) e multiplique 
-todos os valores desse vetor por este escalar inteiro lido. Ao final, mostre na tela todos os valores multiplicados pelo escalar.
-*/
