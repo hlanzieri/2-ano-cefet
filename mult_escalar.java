d95c7189ba31a257bf90684c3024a6b4e7604964
Humberto de Moraes Lanzieri Filho <134455113+hlanzieri@users.noreply.github.com>
2024-03-22 08:36:48 -0300

    Add files via upload

---
+++ mult_escalar.java
@@ -0,0 +1,30 @@
+/*Faça um programa que preencha (via estrutura de repetição) um vetor numérico de tamanho N em ordem crescente. Em seguida receba um valor do teclado (número inteiro) e multiplique 
+todos os valores desse vetor por este escalar inteiro lido. Ao final, mostre na tela todos os valores multiplicados pelo escalar.
+*/
+
+import java.util.Scanner;
+
+public class status_aluno {
+    public static void main(String args[]){
+        Scanner entrada = new Scanner(System.in);
+        int i=0, N=0;
+        System.out.println("Insira o tamanho do vetor:");
+        N = entrada.nextInt();
+        int M[N];
+        System.out.println("Insira "+N+" números em ordem crescente para preencher o vetor:");
+        while(i<N){
+            System.out.println("Insira o "+(i+1)+"° número:");
+            M[i] = entrada.nextInt();
+            i++;
+        }
+        System.out.println("Vetor preenchido!");
+        System.out.println("Insira um número:");
+        int x = entrada.nextInt();
+        for(i=0; i<N; i++){
+            int y=0;
+            y=x*M[i];
+            System.out.println("A multiplicação do "+(i+1)+"° número com o escalar é igual a "+y);
+        }
+        entrada.close();
+    }
+}
